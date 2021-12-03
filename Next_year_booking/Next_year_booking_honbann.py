2fadd6ca451324bef606d61659b0567b079a3563
ms-miyazaki <miyazaki@cancerscan.jp>
2021-12-03 13:49:23 +0900

    アルゴリズム_アウトプットまで完成

--- Next_year_booking/Next_year_booking_honbann.py
+++ Next_year_booking/Next_year_booking_honbann.py
@@ -240,6 +240,7 @@ def main():
     inst = _read_timetable()
     target = Person.from_dataframe(person_table)
     inst_t = _transform_inst_table(inst)
+    inst_cap_sum = inst_t[Col.CAP].sum(axis=0)
     determined_target_list = []
     # 優先度つきのキューに変換
     heapify(target)
@@ -251,108 +252,177 @@ def main():
         req = person.priority
         # 希望条件を減らしていく
         for i in range(hope_num, -1, -1):
-            rest_priority = req[:i]
-            available_inst = inst_t[inst_t[Col.TYPE].isin(person.check) & (inst_t[Col.CAP] > 0)]
-            # 健診を受けられる会場がない場合
-            if (available_inst.shape[0] == 0) & (len(req) == 0):
-                human_power_list.append(person)
+            start_check_len = len(person.check)
+            end_check_len = start_check_len + 1
+            print(i)
+            if start_check_len == 0:
                 break
-            if available_inst.shape[0] == 0:
-                continue
-            if Req.INST in rest_priority:
-                available_inst = available_inst[available_inst[Col.NAME].isin(person.regions)]
-            if Req.WEEKDAY in rest_priority:
-                available_inst = available_inst[available_inst[Col.WEEKDAY].isin(person.weekdays)]
-            if Req.MONTH in rest_priority:
-                available_inst = available_inst[available_inst[Col.MONTH].isin(person.months)]
 
-            # 同日で最も多くの検診を受けられる場所と日時を探している。
-            inst_list = []
-            med_list = []
-            counter_set = 0
-            for v, k in zip(available_inst.groupby(by=[Col.NAME, Col.DATE])[Col.TYPE].unique(),
-                            available_inst.groupby(by=[Col.NAME, Col.DATE])[Col.TYPE].unique().index):
-                counter = 0
-                for l in v:
-                    if l in person.check:
-                        counter += 1
-                if counter > counter_set:
+            rest_priority = req[:i]
+            # 同じ希望条件数で複数回割り振りを行いたい
+            while start_check_len != end_check_len:
+                start_check_len = len(person.check)
+                available_inst = inst_t[inst_t[Col.TYPE].isin(person.check) & (inst_t[Col.CAP] > 0)]
+                # 健診を受けられる会場がない場合
+                if Req.INST in rest_priority:
+                    available_inst = available_inst[available_inst[Col.NAME].isin(person.regions)]
+                if Req.WEEKDAY in rest_priority:
+                    available_inst = available_inst[available_inst[Col.WEEKDAY].isin(person.weekdays)]
+                if Req.MONTH in rest_priority:
+                    available_inst = available_inst[available_inst[Col.MONTH].isin(person.months)]
+                if (available_inst.shape[0] == 0) & (len(rest_priority) == 0):
+                    human_power_list.append(person)
+                    break
+                if available_inst.shape[0] == 0:
+                    break
+                if available_inst.groupby(by=[Col.NAME, Col.DATE])[Col.TYPE].nunique().max() < len(person.check):
+                    break
+
+                # 健診項目が1つの場合は1番時間が早い場所に申し込む
+                if start_check_len == 1:
+                    best_inst = available_inst.sort_values(by=[Col.HEAD])[Col.NAME].iloc[0]
+                    best_date = available_inst.sort_values(by=[Col.HEAD])[Col.DATE].iloc[0]
+                    best_med_list = available_inst.sort_values(by=[Col.HEAD])[Col.TYPE].iloc[0]
+                    best_time_list = available_inst.sort_values(by=[Col.HEAD])[Col.HEAD].iloc[0]
+                    _last = available_inst.sort_values(by=[Col.HEAD])[Col.LAST].iloc[0]
+                else:
+                    # 同日で最も多くの検診を受けられる場所と日時を探している。
                     inst_list = []
                     med_list = []
-                    inst_list.append(k)
-                    med_list.append(i)
-                    counter_set = counter
-                elif counter == counter_set:
-                    inst_list.append(k)
-                    med_list.append(i)
-                    assert len(inst_list) == len(med_list)
+                    counter_set = 0
+                    for v, k in zip(available_inst.groupby(by=[Col.NAME, Col.DATE])[Col.TYPE].unique(),
+                                    available_inst.groupby(by=[Col.NAME, Col.DATE])[Col.TYPE].unique().index):
+                        counter = 0
+                        for l in v:
+                            if l in person.check:
+                                counter += 1
+                        if counter > counter_set:
+                            inst_list = []
+                            med_list = []
+                            inst_list.append(k)
+                            med_list.append(v)
+                            counter_set = counter
+                        elif counter == counter_set:
+                            inst_list.append(k)
+                            med_list.append(v)
+                            assert len(inst_list) == len(med_list)
 
-            # なるべく待ち時間が少ない医療機関を選択
-            best_wait_time = 1000000
-            best_time_list = np.nan
-            best_inst = np.nan
-            best_date = np.nan
-            best_med_list = np.nan
-            for p in range(0, len(inst_list)):
-                _name, _date = inst_list[p]
-                _order = med_list[p]
-                # 希望に合わせて絞ったデータフレーム
-                available_inst_sort = available_inst[
-                    (available_inst[Col.NAME] == _name) & (available_inst[Col.DATE] == _date)]
-                early_wait_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].min().diff().abs().max()
-                early_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].min()
-                late_wait_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].max().diff().abs().max()
-                late_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].max()
-                min_wait_time = min(early_wait_time, late_wait_time)
-                if early_wait_time <= late_wait_time:
-                    best_time = early_time
-                else:
-                    best_time = late_time
-                if min_wait_time < best_wait_time:
-                    best_wait_time = min_wait_time
-                    best_inst = _name
-                    best_date = _date
-                    best_med_list = best_time.index
-                    best_time_list = best_time
+                    # なるべく待ち時間が少ない医療機関を選択
+                    best_wait_time = 1000000
+                    best_time_list = np.nan
+                    best_inst = np.nan
+                    best_date = np.nan
+                    best_med_list = np.nan
+                    for p in range(0, len(inst_list)):
+                        _name, _date = inst_list[p]
+                        _order = med_list[p]
+                        # 希望に合わせて絞ったデータフレーム
+                        available_inst_sort = available_inst[
+                            (available_inst[Col.NAME] == _name) & (available_inst[Col.DATE] == _date)]
+                        early_wait_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].min().diff().abs().max()
+                        early_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].min()
+                        late_wait_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].max().diff().abs().max()
+                        late_time = available_inst_sort.groupby(by=[Col.TYPE])[Col.HEAD].max()
+                        min_wait_time = min(early_wait_time, late_wait_time)
+                        if early_wait_time <= late_wait_time:
+                            best_time = early_time
+                        else:
+                            best_time = late_time
+                        if min_wait_time < best_wait_time:
+                            best_wait_time = min_wait_time
+                            best_inst = _name
+                            best_date = _date
+                            best_med_list = best_time.index
+                            best_time_list = best_time
 
-            # 受診日にフラグ立て
-            if type(best_time_list) == list:
-                for m, n in zip(best_med_list, best_time_list):
+                # 受診日にフラグ立て
+                # 健診項目が1つの時
+                if type(best_time_list) == np.int64:
                     available_inst.loc[
                         (available_inst[Col.NAME] == best_inst) & (available_inst[Col.DATE] == best_date) & (
-                                available_inst[Col.TYPE] == m) & (available_inst[Col.HEAD] == n), 'extract'] = 1
-                assert available_inst["extract"].sum(axis=0) == len(best_time_list)
-            else:
-                available_inst.loc[(available_inst[Col.NAME] == best_inst) & (available_inst[Col.DATE] == best_date) & (
-                        available_inst[Col.TYPE] == best_med_list) & (
-                                               available_inst[Col.HEAD] == best_time_list), 'extract'] = 1
+                                available_inst[Col.TYPE] == best_med_list) & (
+                                available_inst[Col.HEAD] == best_time_list), 'extract'] = 1
+                    assert available_inst["extract"].sum(axis=0) == 1
 
-            # 割り振り情報を追加
-            output_df_raw = available_inst[available_inst['extract'] == 1].copy()
-            output_df_raw["id"] = person.id
-            if first_concat == 0:
-                output_df = output_df_raw.copy()
-                first_concat += 1
-            else:
-                output_df = pd.concat([output_df, output_df_raw], axis=0)
+                    # 割り振り情報を追加
+                    output_df_raw = available_inst[available_inst['extract'] == 1].copy()
+                    output_df_raw["id"] = person.id
+                    if first_concat == 0:
+                        output_df = output_df_raw.copy()
+                        first_concat += 1
+                    else:
+                        output_df = pd.concat([output_df, output_df_raw], axis=0)
 
-            for h in range(len(best_time_list)):
-                _head = available_inst[available_inst["extract"] == 1][Col.HEAD].iloc[h]
-                _last = available_inst[available_inst["extract"] == 1][Col.LAST].iloc[h]
-                _type = available_inst[available_inst["extract"] == 1][Col.TYPE].iloc[h]
-                tt = Timetable(
-                    name=best_inst,
-                    date=best_date,
-                    type=_type,
-                    head=_head,
-                    last=_last
-                )
+                    # tt = Timetable(
+                    #     name=best_inst,
+                    #     date=best_date,
+                    #     type=_type,
+                    #     head=_head,
+                    #     last=_last
+                    # )
+                    person.check.remove(best_med_list)
 
-                # 健診機関の健診種別と開始時間・終了時間の情報をPersonに追加、健診機関のcapacityを1減らす
-                person.append_timetable(tt)
-                inst_t.loc[(inst_t[Col.NAME] == best_inst) & (inst_t[Col.DATE] == best_date) &
-                           (inst_t[Col.TYPE] == _type) & (inst_t[Col.HEAD] == _head) &
-                           (inst_t[Col.LAST] == _last), Col.CAP] -= 1
+                    # 健診機関の健診種別と開始時間・終了時間の情報をPersonに追加、健診機関のcapacityを1減らす
+                    # person.append_timetable(tt)
+                    inst_t.loc[(inst_t[Col.NAME] == best_inst) & (inst_t[Col.DATE] == best_date) &
+                               (inst_t[Col.TYPE] == best_med_list) & (inst_t[Col.HEAD] == best_time_list) &
+                               (inst_t[Col.LAST] == _last), Col.CAP] -= 1
+
+                    end_check_len = len(person.check)
+                    if end_check_len == 0:
+                        break
+                # 健診項目が2つ以上の時
+                else:
+                    for m, n in zip(best_med_list, best_time_list):
+                        available_inst.loc[
+                            (available_inst[Col.NAME] == best_inst) & (available_inst[Col.DATE] == best_date) & (
+                                    available_inst[Col.TYPE] == m) & (available_inst[Col.HEAD] == n), 'extract'] = 1
+                    assert available_inst["extract"].sum(axis=0) == len(best_time_list)
+
+                    # 割り振り情報を追加
+                    output_df_raw = available_inst[available_inst['extract'] == 1].copy()
+                    output_df_raw["id"] = person.id
+                    if first_concat == 0:
+                        output_df = output_df_raw.copy()
+                        first_concat += 1
+                    else:
+                        output_df = pd.concat([output_df, output_df_raw], axis=0)
+
+                    for h in range(len(best_time_list)):
+                        _head = output_df_raw[Col.HEAD].iloc[h]
+                        _last = output_df_raw[Col.LAST].iloc[h]
+                        _type = output_df_raw[Col.TYPE].iloc[h]
+                        # tt = Timetable(
+                        #     name=best_inst,
+                        #     date=best_date,
+                        #     type=_type,
+                        #     head=_head,
+                        #     last=_last
+                        # )
+                        person.check.remove(_type)
+
+                        # 健診機関の健診種別と開始時間・終了時間の情報をPersonに追加、健診機関のcapacityを1減らす
+                        # person.append_timetable(tt)
+                        inst_t.loc[(inst_t[Col.NAME] == best_inst) & (inst_t[Col.DATE] == best_date) &
+                                   (inst_t[Col.TYPE] == _type) & (inst_t[Col.HEAD] == _head) &
+                                   (inst_t[Col.LAST] == _last), Col.CAP] -= 1
+
+                    end_check_len = len(person.check)
+                    if end_check_len == 0:
+                        break
+    print(inst_cap_sum)
+    print(inst_t[Col.CAP].sum(axis=0))
+    print(output_df.shape[0])
+    print(output_df.shape[0] + inst_t[Col.CAP].sum(axis=0))
 
 
 main()
+
+# for i in range(10):
+#     abc = [11, 12, 13, 14, 15]
+#     print(i)
+#     while True:
+#         abcd = abc.pop()
+#         print(abcd)
+#         if abcd == 13:
+#             break
